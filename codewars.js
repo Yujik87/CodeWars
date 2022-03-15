e5b6e5c3ea85ad9b72c721acc836a03b490b8913
Yujik <yujika87@gmail.com>
2022-03-15 22:30:25 +0300

    2

--- codewars.js
+++ codewars.js
@@ -1,5 +1,7 @@
 // 8kuy
 
+//Comfortable word
+
 /*const comfortableWord = word => {
     let separetedWord = [...word];
     let left = ['q', 'w', 'e', 'r', 't', 'a', 's', 'd', 'f', 'g', 'z', 'x', 'c', 'v', 'b'];
@@ -25,11 +27,43 @@
     }
 };*/
 
+
 // Square(n) Sum
 
+/*
 function squareSum(numbers){
-    return numbers.reduce((acc, el) => acc + el*el, 0);
+    return numbers.reduce((acc, el) => acc + el**2, 0);
 }
 
 console.log(squareSum([1,2,3]))
-console.log(squareSum([0,3,4,5]))
+console.log(squareSum([0,3,4,5]))*/
+
+
+// Counting sheep...
+
+/*
+function countSheeps(arrayOfSheep) {
+    return arrayOfSheep.reduce((acc, el) => el === true ? acc + 1 : acc, 0)
+}
+
+console.log(countSheeps([true, false, true, true]))*/
+
+//Is n divisible by x and y?
+
+/*
+function isDivisible(n, x, y) {
+     return ((n % x === 0 && n % y === 0) ? true : false)
+}
+
+console.log('12/1/3', isDivisible(12,1,3));*/
+
+
+//Century From Year
+/*
+function century(year) {
+    if (year <= 100) {
+        return 1;
+    } else {
+        return (Math.ceil(year / 100))
+    }
+}*/
